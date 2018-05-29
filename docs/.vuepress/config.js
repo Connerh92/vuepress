31d102e209c39b9237694e564ba80aca31fe0367
Nate Iler <nate@flipboxdigital.com>
2018-05-29 16:39:30 -0600

    adding docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,50 @@
 module.exports = {
     title: 'VuePress Demo',
     description: 'A demo VuePress site',
-    base: '/vuepress/'
+    base: '/vuepress/',
+    themeConfig: {
+        repo: 'craftcms/docs',
+        docsRepo: 'flipboxlabs/vuepress',
+        docsDir: 'docs',
+        docsBranch: 'master',
+        editLinks: true,
+        nav: [
+            {text: 'Details', link: 'https://github.com/craftcms/docs/'},
+            {text: 'Changelog', link: 'https://github.com/craftcms/docs/'},
+            {text: 'Documentation', link: '/'}
+        ],
+        sidebar: {
+            '/': [
+                {
+                    title: 'Introduction',
+                    collapsable: false,
+                    // children: [
+                    //     ''
+                    // ]
+                },
+                {
+                    title: 'Installing / Updating',
+                    collapsable: false,
+                    children: [
+                        'requirements',
+                        'installation'
+                    ]
+                },
+                {
+                    title: 'Configuring',
+                    collapsable: false,
+                    children: [
+                        '/configure/',
+                        '/configure/organizations',
+                        '/configure/organization-types',
+                        '/configure/user-types'
+                    ]
+                }
+            ]
+        },
+        codeLanguages: {
+            twig: 'Twig',
+            php: 'PHP'
+        }
+    }
 }
---
+++ docs/support.md
@@ -0,0 +1,11 @@
+# Support
+
+All **Bugs**, **Enhancements**, **Feature Requests** and other issues are managed through [GitHub](https://github.com/flipboxfactory/organizations/issues).
+
+{% include buttonLinkOutline.html text="View Issues" href="https://github.com/flipboxfactory/organizations/issues" %}
+
+***
+
+Collaborate and get questions answered quickly via [chat](https://gitter.im/flipboxfactory/organizations).
+
+{% include buttonLinkOutline.html text="View Discussions" href="https://gitter.im/flipboxfactory/organizations" %}
